25aa0d66a818c617abbe3407d53020f5075d50b0
sugus.grassi <sugus.grassi@gmail.com>
2020-10-06 00:11:36 -0300

    Updates

---
+++ precache-manifest.eb384a95ac83e90ef5bb502b8c971ff3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3cf576c0e6d1e9cb49b632c1bdc5e23",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "099f3313f446a51c66a4",
+    "url": "/todolist/static/css/main.2c3d4f61.chunk.css"
+  },
+  {
+    "revision": "c1efd1f54ee8d190d555",
+    "url": "/todolist/static/js/2.6ad3fecf.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/todolist/static/js/2.6ad3fecf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "099f3313f446a51c66a4",
+    "url": "/todolist/static/js/main.7405cd42.chunk.js"
+  },
+  {
+    "revision": "4de95af6f84a9e6eba22",
+    "url": "/todolist/static/js/runtime-main.10d587fd.js"
+  }
+]);
